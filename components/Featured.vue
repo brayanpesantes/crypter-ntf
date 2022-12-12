931e80d691b2b4851b032457bcacb0ed42891f54
brayanpesantes <brayalexis.30@gmail.com>
2022-12-12 11:13:20 -0500

    component featured

---
+++ components/Featured.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="space-y-6">
+    <div class="rounded-xl bg-secondary-400">
+      <img
+        src="/images/content/selection-pic-1.jpg"
+        alt=""
+        class="h-[432px] rounded-xl object-cover object-center"
+      />
+    </div>
+    <div class="flex w-full">
+      <div class="flex flex-1 gap-4">
+        <Avatar class="h-12 w-12" src="/images/content/avatar-5.jpg" />
+        <div class="">
+          <h3 class="text-body-2-bold md:text-body-1-bold">
+            The future of ETH®
+          </h3>
+          <p class="text-caption-2-bold md:text-caption-bold text-neutrals-700">
+            18 in stock
+          </p>
+        </div>
+      </div>
+      <div class="text-center">
+        <p class="text-caption-2">Highest bid</p>
+        <Badge text="1.125 ETH" />
+      </div>
+    </div>
+  </article>
+</template>
