6225a0d14c6c664edfd3a1d1601061202c7a9876
brayanpesantes <brayalexis.30@gmail.com>
2022-12-12 11:13:50 -0500

    add section creators

---
+++ components/Creator.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps(["avatar", "name", "money", "post"]);
+</script>
+<template>
+  <article
+    class="flex w-[224px] shrink-0 items-center gap-4 px-1 pt-6 md:pb-[25px]"
+  >
+    <div class="relative block h-14 w-14">
+      <Avatar :src="avatar" class="z-50" />
+      <span
+        class="text-caption-2-bold absolute -top-1 -left-1 box-content flex h-6 w-6 items-center justify-center rounded-[64px] bg-neutrals-800 text-neutrals-100 ring-2 ring-inset ring-neutrals-200"
+      >
+        {{ post }}
+      </span>
+    </div>
+    <div class="">
+      <p class="text-caption-bold">{{ name }}</p>
+      <p class="text-caption-2-bold mt-[2px] text-neutrals-600">
+        {{ money }}<span class="text-caption-2 ml-[2px]">ETH</span>
+      </p>
+    </div>
+  </article>
+</template>
---
+++ components/ListCreators.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { IconArrowRight } from "~/assets/icons";
+const listCreators = [
+  {
+    avatar: "/images/content/avatar-1.jpg",
+    name: "Payton Harris",
+    money: "2,456",
+    post: 6,
+  },
+  {
+    avatar: "/images/content/avatar-2.jpg",
+    name: "Anita Bins",
+    money: "2,456",
+    post: 2,
+  },
+  {
+    avatar: "/images/content/avatar-3.jpg",
+    name: "Joana wuckert",
+    money: "2,456",
+    post: 3,
+  },
+  {
+    avatar: "/images/content/avatar-4.jpg",
+    name: "Lorena Lender",
+    money: "2,456",
+    post: 4,
+  },
+];
+</script>
+<template>
+  <div class="overflow-hidden">
+    <h4 class="text-caption-2-bold">Latest upload from creators 🔥</h4>
+    <div
+      class="flex overflow-x-auto md:flex-row md:justify-between lg:flex-col lg:divide-y lg:divide-neutrals-400"
+    >
+      <Creator
+        v-for="creator in listCreators"
+        :avatar="creator.avatar"
+        :name="creator.name"
+        :post="creator.post"
+        :money="creator.money"
+      />
+    </div>
+    <button class="btn btn-outline-secondary hidden items-center lg:flex">
+      Discover more <IconArrowRight class="ml-3" />
+    </button>
+  </div>
+</template>
