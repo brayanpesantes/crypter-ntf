8eb900e9ce7189ccb925f8d959afcc1fef2dd199
brayanpesantes <brayalexis.30@gmail.com>
2022-12-12 11:07:22 -0500

    add new component badge

---
+++ components/Badge.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(["text"]);
+</script>
+<template>
+  <span
+    class="border-box block px-[6px] pt-[6px] pb-[4px] text-hairline-2 text-primary-400 border-2 border-primary-400 rounded-[4px]"
+  >
+    {{ text }}
+  </span>
+</template>
