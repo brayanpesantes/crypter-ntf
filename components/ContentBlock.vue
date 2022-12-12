15db607b7aafebe55b60d53dcd3ea201324dfb73
brayanpesantes <brayalexis.30@gmail.com>
2022-12-12 11:20:07 -0500

    add section block

---
+++ components/ContentBlock.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="h-64 w-full bg-neutrals-100 flex items-end">
+    <div
+      class="w-[311px] md:w-[864px] lg:w-[736px] h-[172px] md:h-[140px] mx-auto text-center"
+    >
+      <p class="text-hairline-2 text-neutrals-400">
+        Create, explore, & collect digital art NFTs.
+      </p>
+      <p class="text-hairline-3 mt-2">The new creative economy.</p>
+      <button class="btn btn-outline-secondary mt-6">Start your search</button>
+    </div>
+  </section>
+</template>
