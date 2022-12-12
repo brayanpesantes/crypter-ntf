b7a0df6bda751b18717bcc03cc2829bf1efa5b57
brayanpesantes <brayalexis.30@gmail.com>
2022-12-12 11:11:04 -0500

    add new section item list

---
+++ components/Item.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps(["image", "title", "avatar", "mount", "price"]);
+</script>
+<template>
+  <article class="flex w-[351px] items-center gap-6">
+    <img
+      :src="image"
+      :alt="title"
+      class="block h-[148px] w-40 rounded-xl object-cover"
+    />
+    <div class="flex-1">
+      <h3 class="text-body-2-bold break-all">{{ title }}</h3>
+      <div class="mt-2 flex items-center gap-[11px]">
+        <Avatar class="h-6 w-6" :src="avatar" />
+        <Badge :text="mount" />
+        <p class="text-caption text-neutrals-400">1 of {{ price }}</p>
+      </div>
+      <button class="btn-sm btn-outline-secondary mt-4">Place a bid</button>
+    </div>
+  </article>
+</template>
---
+++ components/ItemList.vue
@@ -0,0 +1,37 @@
+<script setup>
+const listItems = [
+  {
+    title: "ETH never die",
+    image: "/images/content/card-pic-5.jpg",
+    avatar: "/images/content/avatar-1.jpg",
+    price: 12,
+    mount: "0.27 ETH",
+  },
+  {
+    title: "Future coming soon",
+    image: "/images/content/card-pic-2.jpg",
+    avatar: "/images/content/avatar-3.jpg",
+    price: 3,
+    mount: "0.27 ETH",
+  },
+  {
+    title: "Elon Musk silver coin 3d print",
+    image: "/images/content/card-pic-3.jpg",
+    avatar: "/images/content/avatar-4.jpg",
+    price: 4,
+    mount: "0.27 ETH",
+  },
+];
+</script>
+<template>
+  <div class="md:flex flex-col gap-8 md:flex-shrink-0 lg:w-[351px]">
+    <Item
+      v-for="item in listItems"
+      :image="item.image"
+      :title="item.title"
+      :avatar="item.avatar"
+      :price="item.price"
+      :mount="item.mount"
+    />
+  </div>
+</template>
